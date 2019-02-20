9c1e4261cdfcaa862782605f014cf71b13e16cc4
Lucas Marandat <lucas@dotmind.io>
2019-02-20 15:17:26 +0100

    V0.0.1

---
+++ server.js
@@ -0,0 +1,46 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 8080;
+const STORAGE_FILENAME = 'store.json';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function readData() {
+    try {
+        const file = fs.readFileSync(STORAGE_FILENAME);
+        return JSON.parse(file);
+    } catch (e) {
+        return {};
+    }
+}
+
+function saveNewData(newData) {
+    const data = readData();
+    const mergedData = { ...data, ...newData };
+    fs.writeFileSync(
+        STORAGE_FILENAME,
+        JSON.stringify(mergedData),
+        { encoding: 'utf8' },
+    );
+}
+
+app.post('/', (req, res) => {
+    const { body } = req;
+
+    saveNewData(body);
+    res.send();
+});
+
+app.get('/', (req, res) => {
+    const { query } = req;
+
+    saveNewData(query);
+    res.send();
+});
+
+app.listen(PORT, () => (
+    console.log(`Fake Endpoint is running on http://localhost:${PORT}`)
+));
